b1773790a994d48a642babe749ef25ad6ba4e227
Pablo Cardozo <pablocyc83@gmail.com>
2020-03-27 22:05:50 -0400

    Querys con argumentos

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,9 @@
 const Query = {
-  hello: (parent, args, ctx, info) => 'Hello World!!',
+  hello: (parent, args, ctx, info) => {
+    const { name  } = args
+
+    return `Hello ${name || 'world'}`
+  },
   quantity: () => 1,
   getUser: () => { return {name: 'Pablo', lastName: 'Cardozo' }},
 }
